632db1ace82f60356f90e103680cfc1d9313fbf3
toChaim <toChaim@gmail.com>
2017-12-13 11:57:50 -0800

    progress. update .gitignore.

---
+++ test/serverRouteTest.js
@@ -0,0 +1,8 @@
+const server = require('../server');
+const request = require('supertest');
+
+describe('RESTful routing.', function() {});
+test('GET to /', () => {
+  expect.assertions(1);
+  return;
+});
