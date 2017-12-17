755c68ffa8f06edd001cc6222db3279f10c31dbd
toChaim <toChaim@gmail.com>
2017-12-16 23:43:35 -0800

    import tests.

--- test/serverRouteTest.js
+++ test/serverRouteTest.js
@@ -1,8 +1,23 @@
-const server = require('../server');
 const request = require('supertest');
+const { expect } = require('chai');
+const { app } = require('../server');
 
-describe('RESTful routing.', function() {});
-test('GET to /', () => {
-  expect.assertions(1);
-  return;
+describe('Check Server Routes', function() {
+  it('GET to /user should return 404', function(done) {
+    request(app)
+      .get('/user')
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(404);
+        done();
+      });
+  });
+
+  it('GET to /users', function(done) {
+    request(app)
+      .get('/users')
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+  });
 });
