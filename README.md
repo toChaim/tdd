65e6d9893c3d54f523a8ca2283b3f9b867efcd50
toChaim <toChaim@gmail.com>
2017-12-24 14:48:01 -0800

    now testing user moddle.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # tdd
 Build a node server using TDD.
+
+#instalation
+1. download repository
+2. npm install
+3. npm start
+
+# terminal commands
+"npm start" to start server.
+"npm test" to run tests.
+"npm run test-with-coverage" to run test and see coverage report.
--- models/index.js
+++ models/index.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
-const { MONGODB_URI, DEVELOPMENT, ENV } = require('../config');
+// const mongoose = require('mongoose');
+// const { MONGODB_URI, DEVELOPMENT, ENV } = require('../config');
 
-mongoose.set('debug', DEVELOPMENT);
-mongoose.Promise = Promise;
-mongoose
-  .connect(MONGODB_URI, { useMongoClient: true })
-  .then(() => {
-    console.log('Database connected.');
-  })
-  .catch(err => {
-    console.log(`database failed to connected. err="${err}"`);
-  });
+// mongoose.set('debug', DEVELOPMENT);
+// mongoose.Promise = Promise;
+// mongoose
+//   .connect(MONGODB_URI, { useMongoClient: true })
+//   .then(() => {
+//     console.log('Database connected.');
+//   })
+//   .catch(err => {
+//     console.log(`database failed to connected. err="${err}"`);
+//   });
 
 module.exports.User = require('./user');
