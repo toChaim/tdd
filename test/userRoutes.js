220149063a8424551181470d573a2f8919648d0e
toChaim <toChaim@gmail.com>
2017-12-27 20:28:34 -0800

    integrationRoutes

---
+++ test/userRoutes.js
@@ -0,0 +1,105 @@
+const request = require('supertest');
+const { expect } = require('chai');
+const mongoose = require('mongoose');
+
+const api = require('../api');
+
+mongoose.Promise = Promise;
+const db = require('../models');
+
+var token;
+var auth = {};
+
+function loginUser(auth) {
+  return function(done) {
+    request(api)
+      .post('/auth/login')
+      .json({
+        username: 'beforeEachTest',
+        password: 'secret'
+      })
+      .expect(200)
+      .end(onResponse);
+
+    function onResponse(err, res) {
+      auth.token = res.body.token;
+      return done();
+    }
+  };
+}
+
+beforeEach(function(done) {
+  db.User
+    .create({ username: 'beforeEachTest', password: 'secret' })
+    .then(function(user) {
+      auth.current_user = user;
+      done();
+    });
+});
+
+beforeEach(loginUser(auth));
+
+afterEach(function(done) {
+  db.User.remove({}).then(function() {
+    done();
+  });
+});
+
+describe('POST /auth/signup', function() {
+  it('responds with JSON when created', function(done) {
+    request(api)
+      .post('/auth/signup')
+      .send({ username: 'test1', password: 'secret' })
+      .set('Accept', 'application/json')
+      .expect(200, done);
+  });
+});
+
+describe('POST /auth/login', function() {
+  it('responds with JSON', function(done) {
+    request(api)
+      .post('/auth/login')
+      .send({ username: 'postTest', password: 'secret' })
+      .set('Accept', 'application/json')
+      .expect(201, done);
+  });
+});
+
+describe('GET /users', function() {
+  it('responds with JSON', function(done) {
+    request(api)
+      .get('/users')
+      .set('Accept', 'application/json')
+      .expect(200, done);
+  });
+});
+
+describe('GET /users/:username', function() {
+  it('responds with JSON', function(done) {
+    request(api)
+      .get(`/users/${auth.current_user.username}`)
+      .set('Authorization', `bearer: ${auth.token}`)
+      .expect(200, done);
+  });
+});
+
+describe('PATCH /users/:username', function() {
+  it('responds with JSON', function(done) {
+    request(api)
+      .patch(`/users/${auth.current_user.username}`)
+      .send({
+        username: 'patchTest'
+      })
+      .set('Authorization', `bearer: ${auth.token}`)
+      .expect(200, done);
+  });
+});
+
+describe('DELETE /users/:username', function() {
+  it('responds with JSON', function(done) {
+    request(api)
+      .delete(`/users/${auth.current_user.username}`)
+      .set('Authorization', `bearer: ${auth.token}`)
+      .expect(204, done);
+  });
+});
