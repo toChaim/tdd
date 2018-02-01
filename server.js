76fec950d33ac75e13b24ed2ef60faf5f51c068e
toChaim <toChaim@gmail.com>
2018-02-01 15:16:42 -0800

    clean slate.

--- server.js
+++
@@ -1,8 +0,0 @@
-const api = require('express')();
-const { PORT } = require('./config');
-
-api.get('/users', function(req, res) {
-  res.status(200).json({ name: 'tobi' });
-});
-
-module.exports = { api };
