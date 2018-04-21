0dd68c0d97a3e3ef560be8401eb5bbb25219dd6c
toChaim <toChaim@gmail.com>
2018-04-20 22:52:38 -0700

    new config style

--- config.js
+++ config.js
@@ -1,3 +1,16 @@
-const PORT = process.env.PORT || 3001;
+const getPort = (envPort) => envPort || 3000;
+const getEnv = (nodeEnv) => nodeEnv || 'development';
+const getDB = (dbConnection) => {
+    // mock database
+    if(dbConnection === 'unitTest'){
+        return {};
+    }
+    // integration test
+    if(dbConnection === undefined){
+        dbConnection = 'testDataBaseName';
+    }
+    // connect to database
 
-module.exports = { PORT };
+}
+
+module.exports = { getPort, getEnv, getDB };
