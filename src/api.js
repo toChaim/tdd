307f1603812ad3160fd9a7aea57342686a7c2f93
Chaim Finkelman <toChaim@gmail.com>
2018-05-07 21:41:21 -0700

    pass firts route tests

--- src/api.js
+++ src/api.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import notFoundError from './helpers/notFoundError';
 
 export default (NODE_ENV)=>{
     const api = express();
+
+    api.get('/', (req,res,next)=>{
+        return res.json({users: '/users'});
+    });
+
+    api.use(notFoundError);
+
+    api.use((err, req, res, next) => {
+        return res.status(err.status || 500).json({
+          message: err.message,
+          error: NODE_ENV === "development" ? err : {}
+        });
+      });
 
     return api;
 }
