ae99706156abd2743755fbd9cf59cf38ee88bffb
Chaim Finkelman <toChaim@gmail.com>
2018-05-07 21:23:40 -0700

    should / route and 404

--- test/api.test.js
+++ test/api.test.js
@@ -1,4 +1,5 @@
 import api from '../src/api';
+import request from 'supertest';
 
 describe('api tests', ()=>{
     it('should be a function',()=>{
@@ -10,4 +11,19 @@ describe('api tests', ()=>{
         expect(typeof server).toEqual('function');
     });
 
+    describe('should return correct results', ()=>{
+        const server = api('test');
+        it('/ responds with 200 JSON', function(done) {
+            request(server)
+                .get('/')
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+        it('/not_real_rout responds with 404 JSON', function(done) {
+            request(server)
+                .get('/not_real_rout')
+                .expect('Content-Type', /json/)
+                .expect(404, done);
+        });
+    });
 });
