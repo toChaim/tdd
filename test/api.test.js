cd19f4b5e4c30a2c52dcc5462bff0a83e52067b1
Chaim Finkelman <toChaim@gmail.com>
2018-05-07 21:04:53 -0700

    api() should return a funcion

--- test/api.test.js
+++ test/api.test.js
@@ -3,5 +3,11 @@ import api from '../src/api';
 describe('api tests', ()=>{
     it('should be a function',()=>{
         expect(typeof api).toEqual('function');
-    })
+    });
+
+    it('should return a function',()=>{
+        const server = api('test');
+        expect(typeof server).toEqual('function');
+    });
+
 });
